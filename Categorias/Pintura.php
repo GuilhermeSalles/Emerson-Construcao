d56f73cb1e45dcf6c4355d10f52efe3ba5c8a11a
GuilhermeSalles <guibaltazarvs@gmail.com>
2024-07-30 12:12:16 -0300

    preços atualizados, e produtos adicionados

--- Categorias/Pintura.php
+++ Categorias/Pintura.php
@@ -18,7 +18,7 @@
 
                 </div>
                 <div class="card-footer text-center">
-                    <h4>R$ 149,90 à vista</h4>
+                    <h4>R$ 169,00 à vista</h4>
                     <a type="button" class="btn btn-lg btn-block mt-3" style="background-color: #13AC2D; color: white;" href="https://wa.me/5519974114770?text=Ol%C3%A1,%20Gostaria%20de%20falar%20sobre%20a%20categoria%20de%20Tintas%20que%20tem%20no%20site!">Comprar!</a>
                 </div>
 
