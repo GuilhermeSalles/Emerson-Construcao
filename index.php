2a9910de965a150f78c4b1b96f1eda31f2b2ecda
GuilhermeSalles <guibaltazarvs@gmail.com>
2024-10-28 08:06:20 -0300

    changed images

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
         <div class="carousel-caption d-none d-md-block">
         </div>
       </div>
-      <div class="carousel-item">
+      <!-- <div class="carousel-item">
         <img src="img/bannerComputador/3.png" class="d-block w-100 img-fluid pc" alt="Imagem carrosel promoção" style="height: 820px;">
         <div class="carousel-caption d-none d-md-block">
         </div>
-      </div>
+      </div> -->
       <div class="carousel-item active">
         <a href="ofertas"><img src="img/bannerComputador/2.png" class="d-block w-100 img-fluid pc" alt="Imagem carrosel promoção" style="height: 820px;"></a>
       </div>
